1fe4e8b1f22b958d45ed68b618aaa77657452f6d
Mika Nokka <mika.nokka1@gmail.com>
2020-01-16 14:13:30 +0200

    Username to possible Jira account mapping works

--- krono2jira.py
+++ krono2jira.py
@@ -26,30 +26,37 @@ __version__ = u"0.9.KRONODOC"
 
 
 ###################################################################
-# should pass via parameters
+# TODO: should pass via parameters
 # CODE CONFIGURATIONS
 #####################################################################
+
+logging.basicConfig(level=logging.DEBUG) # IF calling from Groovy, this must be set logging level DEBUG in Groovy side order these to be written out
 
 # development vs production Jira
 #ENV="DEV"
 ENV="PROD"
 
-#risk vs mitigation risk project operations
-#TYPE="MITI"
-TYPE="RISK"
-
-#project type ship vs. finance
-#CATE="SHIP"
-CAT="FIN"
 
 # do only one operation for testing purposes
 ONCE="NO"
 #ONCE="YES"
 
-###########################################################################
+
+##################################################
+#CONFIGURATIONS AND EXCEL COLUMN MAPPINGS
+
+DATASTARTSROW=2000  # 2 # data section starting line 
+J=10 # this column holds the kronodoc exporter usernames
+MainSheet="CCL2"    # HARDCODED SHEET 
 
 
-logging.basicConfig(level=logging.DEBUG) # IF calling from Groovy, this must be set logging level DEBUG in Groovy side order these to be written out
+####END OF CONFIGURATIONS #######################################################################
+
+
+
+
+
+
 
 
 def main():
@@ -102,35 +109,36 @@ def main():
         #print "args: {0}".format(args)
         sys.exit(2)
 
+    
+    start = time.clock()
     # python krono2jira.py -s http://localhost:8080 -u mika.nokka@ambientia.fi -w kissa -q . -n kissa -p kissa
     Authenticate(JIRASERVICE,PSWD,USER)
     jira=DoJIRAStuff(USER,PSWD,JIRASERVICE)
     
     ###############################################################################
-    
+    # Testing name -> jira useraccount mapping theory
     #found example
-    user="nokka, mika"
-    account=CheckExcelName(jira,user)
+    #user="nokka, mika"
+    #account=CheckExcelName(jira,user)
     
     #not found example
     #user="noskka, mikfa"
     #account=CheckExcelName(jira,user)
     
-    if (account != "NOT EXIST"):
-        logging.debug("User:{0} ---> Jira Account:{1}".format(user,account))
-    else:
-        logging.debug("User:{0} ---> NO Jira account".format(user,account))
+    #if (account != "NOT EXIST"):
+    #    logging.debug("User:{0} ---> Jira Account:{1}".format(user,account))
+    #else:
+    #    logging.debug("User:{0} ---> NO Jira account".format(user,account))
     
     ###########END OF POC CODE ##################################################
-    print "EXITING NOW!!"
-    sys.exit(5)
+    
     
     
     excel=excelfilepath+"/"+filename
     logging.debug ("Excel file:{0}".format(excel))
 
     Issues=defaultdict(dict) 
-    MainSheet="Sheet0" 
+
     wb= openpyxl.load_workbook(excel)
     #types=type(wb)
     #logging.debug ("Type:{0}".format(types))
@@ -139,313 +147,73 @@ def main():
     CurrentSheet=wb[MainSheet] 
     
     
-    ########################################
-    #CONFIGURATIONS AND EXCEL COLUMN MAPPINGS, both main and subtask excel
-    DATASTARTSROW=4 # data section starting line 
-    A=1 #issuetype
-    E=5 #SUMMARY
-    F=6 #priority
-    H=8 #Status   
-    Q=17 #Assignee
-    S=19 #Disciopline(F)
+
     
-    T=20 #Probability
-    U=21 #HSE Impact
-    V=22 #Schedule Impact 
-    W=23 #Quality Impact
-    
-    Z=26 #Risk Cost
-    AK=37 #Linked Issues
-    AM=39 ##Disciopline(RM)
-    AN=40 #Description
-    AB=28 #Mitigation Costs (Keur)
-    
- 
-    print "=====>    Internal configuration:{0} , {1} , {2}".format(ENV, TYPE, CAT)
+    print "=====>    Internal configuration:{0} , {1} ".format(ENV, ONCE)
  
     ##############################################################################################
-    #Go through main excel sheet for main issue keys (and contents findings)
-    # Create dictionary structure (remarks)
+    # Go through main excel sheet and find people names, ask Jira matching useraccount info
     # NOTE: Uses hardcoded sheet/column values
     # NOTE: As this handles first sheet, using used row/cell reading (buggy, works only for first sheet) 
     #
     i=DATASTARTSROW # brute force row indexing
-    for row in CurrentSheet[('C{}:C{}'.format(DATASTARTSROW,CurrentSheet.max_row))]:  # go trough all column C  rows (issue key when imp exp eported)
+    for row in CurrentSheet[('J{}:J{}'.format(DATASTARTSROW,CurrentSheet.max_row))]:  # go trough all column J  rows 
         for mycell in row:
-            KEY=mycell.value
-            logging.debug("ROW:{0} Original ID:{1}".format(i,mycell.value))
-            Issues[KEY]={} # add to dictionary as master key (KEY)
+            NAME=mycell.value
+            logging.debug("ROW:{0} Name:{1}".format(i,mycell.value))
+            Issues[NAME]={} # add to dictionary as master key (PERSON)
             
-            #Just hardocode operations, POC is one off
-            #LINKED_ISSUES=(CurrentSheet.cell(row=i, column=K).value) #NOTE THIS APPROACH GOES ALWAYS TO THE FIRST SHEET
-            #logging.debug("Attachment:{0}".format((CurrentSheet.cell(row=i, column=K).value))) # for the same row, show also column K (LINKED_ISSUES) values
-            #Issues[KEY]["LINKED_ISSUES"] = LINKED_ISSUES
+            #Just hardocoded operations, POC is one off
             
-            SUMMARY=(CurrentSheet.cell(row=i, column=E).value)
-            if not SUMMARY:
-                SUMMARY="Summary for this task has not been defined"
-            Issues[KEY]["SUMMARY"] = SUMMARY
+            THENAME=(CurrentSheet.cell(row=i, column=J).value)
+            if not THENAME:
+                THENAME="NO_NAME"
+            Issues[NAME]["THENAME"] = THENAME
             
-            ISSUE_TYPE=(CurrentSheet.cell(row=i, column=A).value)
-            Issues[KEY]["ISSUE_TYPE"] = ISSUE_TYPE
+            account=CheckExcelName(jira,THENAME)
+            Issues[NAME]["ACCOUNT"] = account
             
-            STATUS=(CurrentSheet.cell(row=i, column=E).value)
-            Issues[KEY]["SUMMARY"] = SUMMARY
-            
-            PRIORITY=(CurrentSheet.cell(row=i, column=F).value)
-            Issues[KEY]["PRIORITY"] = PRIORITY
-           
-            
-            STATUS=(CurrentSheet.cell(row=i, column=H).value)
-            Issues[KEY]["STATUS"] = STATUS
-            
-            
-            ASSIGNEE=(CurrentSheet.cell(row=i, column=Q).value)
-            Issues[KEY]["ASSIGNEE"] = ASSIGNEE
-            
-            DisciplineF=(CurrentSheet.cell(row=i, column=S).value)
-            Issues[KEY]["DisciplineF"] = DisciplineF
-            
-            DisciplineRM=(CurrentSheet.cell(row=i, column=AM).value)
-            Issues[KEY]["DisciplineRM"] = DisciplineRM
-            
-            DESCRIPTION=(CurrentSheet.cell(row=i, column=AN).value)
-            Issues[KEY]["DESCRIPTION"] = DESCRIPTION
-            
-            PROBABILITY=(CurrentSheet.cell(row=i, column=T).value)
-            Issues[KEY]["PROBABILITY"] = PROBABILITY
-            
-            HSEImpact=(CurrentSheet.cell(row=i, column=U).value)
-            Issues[KEY]["HSEImpact"] = HSEImpact
-            
-              
-            #RESPHONE=(CurrentSheet.cell(row=i, column=U).value)
-            #Issues[KEY]["RESPHONE"] = RESPHONE
-            
-            SheduleImpact=(CurrentSheet.cell(row=i, column=V).value)
-            Issues[KEY]["SheduleImpact"] = SheduleImpact
-            
-            QualityImpact=(CurrentSheet.cell(row=i, column=W).value)
-            Issues[KEY]["QualityImpact"] = QualityImpact
-            
-            RiskCost=(CurrentSheet.cell(row=i, column=Z).value)
-            Issues[KEY]["RiskCost"] = RiskCost
-        
-            MitigationCostsKeur=(CurrentSheet.cell(row=i, column=AB).value)
-            Issues[KEY]["MitigationCostsKeur"] = MitigationCostsKeur
-        
-                
-            LinkedIssues=(CurrentSheet.cell(row=i, column=AK).value)
-            Issues[KEY]["LinkedIssues"] = LinkedIssues
-            
-            
-
             logging.debug("---------------------------------------------------")
             i=i+1
     
     #print Issues.items() 
   
-    #print "priority after all settings:{0}".format(PRIORITY)  
+  
+
+
     for key, value in Issues.iteritems() :
-        print "\n\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-        print "ORIGINAL ISSUE KEY: {0}".format(key)
-        #print key, value
+        logging.info("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+        #print "The name: {0}".format(key)
+        #print key, ">>>", value
         
-        # check linked issues values form each issue found from excel
         one=Issues.get(key)
-        for key, value in one.iteritems() :
-            print "************************************************************************"
-            if (value==None):
-                if (isinstance(value, long)):
-                    castedValue=0
-                    print "NONE NUMBER"
-                else:
-                    castedValue=0
-                    print "NONE" 
-            else:
-                if (isinstance(value, long)): # is it number??
-                    castedValue=value # numbers dont need utf-8 endocing
-                else:
-                    castedValue=value.encode('utf-8') 
-                  
-            
-            #print "value:{0}".format(value)
-            print "{0} {1}".format(key,castedValue)
-            #print "priority in loop:{0}".format(PRIORITY)  
-            
-            if (LINKS and key=="LinkedIssues"): #-l parameter to do links operations to given target project
-                #print "Linked issues column found"
-                
-                TOLINKLIST=[] # all to be linked issues for this issue stored here for processing
-                print "Linking active: Linking target project: {0}".format(LINKS)
-                if (value==None): #no linked items case
-                    value2="NONE"
-                    print "No linked issues found"
-                else:
-                    value2=value
-                onelink=value2.split(':')
-                for item in onelink :
-                    #print "value:{0}".format(item.encode('utf-8'))
-                    regex = r"(.*)(')(.*)(')"   #TT1400-39 'Logistic plan to do' (Risk Mitigation)
-                    match = re.search(regex, item)
-                
-                    if (match):
-                        hit=match.group(3)
-                        #print "-----------------------------------------------------------"
-                        print "Linked issue Summmary ==>  {0}".format(hit.encode('utf-8'))
-                        #print "-----------------------------------------------------------"
-                        
-                        #project = "Risk Mitigation Panel Line"  and summary ~ "Kuitulaser hankinta ja hitsauslaboratorion hankinta"
-                        #issue_list = jira.search_issues("Project = {0} and Summary ~ {1}".format(LINKS,hit))
-                        
-                        jql_query="Project = \'{0}\' and Summary ~ \'{1}\'".format(LINKS,hit.encode('utf-8'))
-                        #print "Query:{0}".format(jql_query)
-                        
-                        issue_list=jira.search_issues(jql_query)
-                        
-                        if len(issue_list) >= 1:
-                            for issue in issue_list:
-                                #logging.debug("One issue returned for query")
-                                logging.debug("ISSUE TO BE LINKED ==> {0}".format(issue))
-                                LINKEDISSUE=issue
-                                TOLINKLIST.append(issue)
-                        #elif len(issue_list) > 1:
-                        #    logging.debug("ERROR ==> More than 1 issue was returned by JQL query")
-                        #    LINKEDISSUE="EMPTY"
-                        else:
-                            logging.debug("==> No issue(s) returned by JQL query")
-                            #LINKEDISSUE="EMPTY"
-            #else:
-            #    LINKEDISSUE="EMPTY"               
-                        
-                
-            if (key=="ASSIGNEE"):
-                #print "Assignee column found"
-                
-                if (value==None): #no linked items case
-                    value2="NONE"
-                    print "No assignee found"
-                    USERNAME_ASSIGNEE="-1"
-                else:
-                    value2=value
-                    regex = r"(.*)(\()(.*)(\))"   #Korpela, Matias (korpma11)
-                    match = re.search(regex, value2)
-                
-                    if (match):
-                        USERNAME_ASSIGNEE=match.group(3).encode('utf-8')
-                        #print "-----------------------------------------------------------"
-                        print "Assignee username ==>  {0}".format(USERNAME_ASSIGNEE)
-                        #print "-----------------------------------------------------------"
-                    else:
-                        USERNAME_ASSIGNEE="-1"
-                        
-                
-            if (key=="MitigationCostsKeur"):
-                #print "Mitigation cost column found"
-                MitigationCostsKeur=castedValue
-                       
-            if (key=="STATUS"):
-                #print "STATUS cost column found: {0}".format(value)
-                if (ENV =="DEV" and TYPE=="MITI"):
-                    if (value=="To Do"):
-                        print "Dev: To Do found, doing nothing"
-                        NEWSTATUS="To Do"
-                    else:
-                        NEWSTATUS=value  
-                        print "Dev: new status set:{0}".format(NEWSTATUS)
-                
-                elif (ENV =="DEV" and TYPE=="RISK"):
-                    if (value=="Proposed"):
-                        print "Dev: TProposed found, doing nothing"
-                        NEWSTATUS="Proposed"
-                    else:
-                        NEWSTATUS=value  
-                        print "Dev: new status set:{0}".format(NEWSTATUS)
-                
-                #duplicating intentionally       
-                elif (ENV =="PROD" and TYPE=="MITI"):
-                    if (value=="To Do"):
-                        print "Prod: To Do found, doing nothing"
-                        NEWSTATUS="To Do"
-                    else:
-                        NEWSTATUS=value  
-                        print "Prod: new status set:{0}".format(NEWSTATUS)
-                
-                elif (ENV =="PROD" and TYPE=="RISK"):
-                    if (value=="Proposed"):
-                        print "Prod: TProposed found, doing nothing"
-                        NEWSTATUS="Proposed"
-                    else:
-                            NEWSTATUS=value  
-                            print "Prod: new status set:{0}".format(NEWSTATUS)
-            
-                else:
-                    print "ERROR: NO STATUSES"
-                    sys.exit(5)        
-                    #TODO STATUSES"    
-                
-                    
-                    
-                    
-            
-            if (key=="SUMMARY"):
-                SUMMARY=castedValue  
-            
-            if (key=="ISSUE_TYPE"):
-                ISSUE_TYPE=castedValue 
-                
-            if (key=="PRIORITY"):
-                PRIORITY=castedValue    
-            
-            if (key=="DESCRIPTION"):
-                DESCRIPTION=castedValue 
-                
-            if (key=="DisciplineRM"):
-                if(castedValue==0):
-                    DisciplineRM=castedValue
-                else:
-                    DisciplineRM="-1"     
-            
-            if (key=="DisciplineF"):
-                if(castedValue==0):
-                    DisciplineF=castedValue     
-                else:
-                    DisciplineF="-1"  
-            
-            
-            if (CAT=="SHIP"):
-                DISCIPLINE=DisciplineRM
-            elif (CAT=="FIN"):
-                DISCIPLINE=DisciplineF
-            
-            if (DISCIPLINE==0):
-                DISCIPLINE="-1"   
-            
-            if (key=="RiskCost"):
-                RiskCost=castedValue   
-                
-             
-            
-                        
-        # just 2 funcitons for 2 projectypes, this is just a tool
-        if (TYPE=="MITI"):
-            CreateMitigationIssue(jira,JIRAPROJECT,SUMMARY,ISSUE_TYPE,PRIORITY,STATUS,USERNAME_ASSIGNEE,DESCRIPTION,MitigationCostsKeur,NEWSTATUS,ENV,DISCIPLINE,CAT)
-        elif (TYPE=="RISK"):
-            CreateRiskIssue(jira,JIRAPROJECT,SUMMARY,ISSUE_TYPE,PRIORITY,STATUS,USERNAME_ASSIGNEE,DESCRIPTION,MitigationCostsKeur,NEWSTATUS,ENV,DISCIPLINE,TYPE,RiskCost,CAT,TOLINKLIST,LINKS)
+        #for key, value in one.iteritems() :        
+            #print key, "---->", value
+            #if (key=="ACCOUNT" and value=="NOT EXIST"):
+            #    logging.debug("ACCOUNTTIII EI OLE:{0}").format(THENAME)
+        ACCOUNT=one.get('ACCOUNT') 
+        #logging.debug("ACCOUNT:{0}".format(ACCOUNT))
+        THENAME=one.get('THENAME')   
+        #logging.debug("THENAME:{0}".format(THENAME))
+        if (ACCOUNT=="NOT EXIST"):
+            logging.info("{0} has NO Jira account".format(THENAME)) 
         else:
-            print "Lost in translation. Cant do want I should do"
+            logging.info("{0} --> Jira account: {1}".format(THENAME,ACCOUNT))      
+            
+            
                 
-        time.sleep(0.7) # prevent jira crashing for script attack
+        time.sleep(0.3) # prevent jira crashing for script attack
         if (ONCE=="YES"):
             print "ONCE testing mode ,stopping now"
             sys.exit(5) #testing do only once
-        print "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-        #now excel has been prosessed
+        #print "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+        #now excel has been processed
         
     end = time.clock()
     totaltime=end-start
-    print "Time taken:{0} seconds".format(totaltime)
-    print "*************************************************************************"
+    logging.info("\n*************************************************************************")
+    logging.info("Time taken:{0} seconds".format(totaltime))
+    logging.info("*************************************************************************")
     sys.exit(0)
     
 
@@ -463,7 +231,7 @@ def CheckExcelName(jira,user):
     result=jira.search_users(user, startAt=0, maxResults=50, includeActive=True, includeInactive=False) #returns dictionary
     account ="NOT EXIST"
     # THIS WORKS
-    print "----------------------------------------------------"
+    logging.debug ("--------------------------------------------------------------------")
     
     if not result:
         logging.debug ("NO JIRA account MATCH for name:{0}".format(user))
@@ -487,7 +255,7 @@ def CheckExcelName(jira,user):
             return account
         #else:
             #print "no match"
-    print "----------------------------------------------------"
+    logging.debug ("------------------------------------------------------------------")
     
     
     
